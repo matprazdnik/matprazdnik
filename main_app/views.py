57e8afa438e6c9440586fc0df0aa4d7581949a50
Cxielamiko@gmail.com <Cxielamiko@gmail.com@2acd3f06-0ca4-0040-da78-bb6aad6ddc83>
2013-02-02 21:59:49 +0000

    sketch design
    git-svn-id: http://matprazdnik.googlecode.com/svn/trunk@6 2acd3f06-0ca4-0040-da78-bb6aad6ddc83

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,17 @@
 # Create your views here.
 from django.shortcuts import render
 
+from main_app.models import Participant, School
+
+
+def attach_info(dict_):
+    return dict({
+        'num_participants': len(Participant.objects.all()),
+    }, **dict_)
+
+
+
 def participants(request):
-    return render(request, 'participants.html')
+    return render(request, 'participants.html', attach_info({
+        'nav': 'participants',
+    }))
